6d60b1eb2826bf738044f23cb94567013065f6a3
justincavalli <justincavalli@yahoo.com>
2023-06-12 13:21:26 -0700

    styling playlist-page

--- client/src/app/components/vega-viz/vega-viz.component.ts
+++ client/src/app/components/vega-viz/vega-viz.component.ts
@@ -11,19 +11,38 @@ export class VegaVizComponent implements OnInit {
     const spec = {
       "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
       "description": "A scatterplot showing each track's tempo and energy level.",
-      "title": "Example Vega-lite Chart",
+      "title": {
+        "text": "Tracks Energy and Tempo",
+        "color": "white",
+        "fontSize": 22
+      },
+      "width": 400,
+      "height": 325,
       "data": {
         "values": [
-          {"Name": "Track 1", "Tempo": 0.23, "Energy": 0.28}, {"Name": "Track 4", "Tempo": 0.11, "Energy": 0.55}, {"Name": "Track 7", "Tempo": 0.31, "Energy": 0.43},
-          {"Name": "Track 2", "Tempo": 0.45, "Energy": 0.91}, {"Name": "Track 5", "Tempo": 0.88, "Energy": 0.81}, {"Name": "Track 8", "Tempo": 0.85, "Energy": 0.53},
-          {"Name": "Track 3", "Tempo": 0.12, "Energy": 0.19}, {"Name": "Track 6", "Tempo": 0.77, "Energy": 0.87}, {"Name": "Track 9", "Tempo": 0.44, "Energy": 0.52}
+          {"Name": "Track 1", "Tempo": 2.3, "Energy": 2.8}, {"Name": "Track 4", "Tempo": 1.1, "Energy": 5.5}, {"Name": "Track 7", "Tempo": 3.1, "Energy": 4.3},
+          {"Name": "Track 2", "Tempo": 4.5, "Energy": 9.1}, {"Name": "Track 5", "Tempo": 8.8, "Energy": 8.1}, {"Name": "Track 8", "Tempo": 8.5, "Energy": 5.3},
+          {"Name": "Track 3", "Tempo": 1.2, "Energy": 1.9}, {"Name": "Track 6", "Tempo": 7.7, "Energy": 8.7}, {"Name": "Track 9", "Tempo": 9.4, "Energy": 5.2}
         ]
       },
-      "mark": "circle",
+      "mark": {
+        "type": "circle",
+        "color": "#1db954",
+        "size": 100
+      },
       "encoding": {
-        "x": {"field": "Tempo", "type": "quantitative"},
-        "y": {"field": "Energy", "type": "quantitative"},
+        "x": {"field": "Tempo", "type": "quantitative", "axis": {"grid": false}},
+        "y": {"field": "Energy", "type": "quantitative", "axis": {"grid": false}},
         "tooltip": {"field": "Name", "type": "nominal"}
+      },
+      "config": {
+        "background": "#212121",
+        "axis": {
+          "titleColor": "white",
+          "titleFontSize": 18,
+          "titlePadding": 8,
+          "labelColor": "white",
+        }
       }
     } as const;
 
